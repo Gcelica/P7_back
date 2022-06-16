d09004df624eaf7ee5cae3f5e5fe82a48ede00a5
Celica Galindo <celica.galindo@gmail.com>
2022-06-16 19:44:29 +0200

    routes utlisateur a verifier, probleme avec les infos reçu dans la requete pour trouver un utilisateur

---
+++ models/User.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+const uniqueValidator = require("mongoose-unique-validator");
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    min: 3,
+    max: 20,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    max: 50,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 6,
+  },
+  profilePicture: {
+    type: String,
+    default: "",
+  },
+  coverPicture: {
+    type: String,
+    default: "",
+  },
+  followers: {
+    type: Array,
+    default: [],
+  },
+  followins: {
+    type: Array,
+    default: [],
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  },
+  desc: {
+    type: String,
+    max: 50,
+  },
+});
+
+UserSchema.plugin(uniqueValidator);
+
+module.exports = mongoose.model("User", UserSchema);
---
+++ routes/users.js
@@ -0,0 +1,103 @@
+const router = require("express").Router();
+const User = require("../models/User");
+const bcrypt = require("bcrypt");
+
+// mise à jour utilisateur
+router.put("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      //mot de passe envoyé
+      try {
+        const salt = await bcrypt.genSalt(10); // nouveau mot de passe generé
+        req.body.password = await bcrypt.hash(req.body.password, salt); //hachage et mise à jour du mot de passe dans le body
+      } catch (err) {
+        return res.status(500).json(err);
+      }
+    }
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+      res.status(200).json("Compte mis à jour");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res
+      .status(403)
+      .json("vous ne pouvez pas mettre à jour votre compte !");
+  }
+});
+
+//supprimer utlisateur
+router.delete("/:id", async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      const user = await User.findByIdAndDelete(req.params.id);
+      res.status(200).json("Compte supprimé");
+    } catch (err) {
+      return res.status(500).json(err);
+    }
+  } else {
+    return res.status(403).json("vous ne pouvez pas supprimer ce compte !");
+  }
+});
+
+//chercher un utilisateur
+router.get("/:id", async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    //objet avec les propriétés non utiles/ utiles
+    const { password, ...other } = user._doc; //_doc contient other
+    res.status(200).json(user);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+// chercher tous les utlisateurs
+
+//suivre un utlisateur
+router.put("/:id/follow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+        res.status(200).json("vous suivez cet utilisateur");
+      } else {
+        res.status(403).json("vous suivez deja cet utilisateur");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("vous ne pouvez pas vous suivre");
+  }
+});
+
+//ne pas suivre un utilisateur
+router.put("/:id/unfollow", async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+        res.status(200).json("user has been unfollowed");
+      } else {
+        res.status(403).json("you dont follow this user");
+      }
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("you cant unfollow yourself");
+  }
+});
+
+module.exports = router;
